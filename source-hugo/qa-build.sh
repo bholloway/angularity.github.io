6054c54b208aa495b0906f1cb502489c086a6090
benholloway <ben.holloway@3plearning.com>
2015-02-12 12:01:30 +1100

    minor fix to bash script

--- source-hugo/qa-build.sh
+++ source-hugo/qa-build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # uses hugo to build with base tag
-
-hugo -b="http://demo.$1.com/static.$1.com/angularity.github.io/"
+echo hugo -b"http://demo.$1.com/static.$1.com/angularity.github.io/"
+hugo -b"http://demo.$1.com/static.$1.com/angularity.github.io/"
